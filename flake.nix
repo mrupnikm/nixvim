40560b08ce00907275d71c4e5f7420c7f22dc9da
Matic Rupnik <m@r.si>
2025-12-03 09:10:27 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "xhuyz nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      nixvim,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      config = import ./config  ;
+
+      nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+        inherit pkgs;
+        module = config;
+      };
+    in
+    {
+      packages.${system}.default = nvim;
+
+    };
+}
